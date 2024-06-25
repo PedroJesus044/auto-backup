e3a8fdc4d639ac3f5de6899abb470d35c18213cc
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-06-25 13:27:35 -0600

    Todo OK

--- frontend-controladora-js/src/components/BackupsList.vue
+++ frontend-controladora-js/src/components/BackupsList.vue
@@ -49,7 +49,7 @@
           </li>
         </ul>
       </div>
-      <form class="col-md-4" action="/" @submit="addMetadata()">
+      <div class="col-md-4">
         <div v-if="currentBackup">
             <h4>Metadata</h4>
             <div>
@@ -137,7 +137,7 @@
           <div v-else>
             <div>
                 <label><strong>Action:</strong></label><br>
-                <button type="submit" class="btn btn-info">
+                <button type="submit" class="btn btn-info" @click="addMetadata()">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-floppy" viewBox="0 0 16 16">
                     <path d="M11 2H9v3h2z"/>
                     <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"/>
@@ -153,7 +153,7 @@
               </div>
           </div>
         </div>
-    </form>
+    </div>
       <div v-if="currentBackup" class="col">
         <div v-if="backup_traces">
             <h4>Stats</h4>
