9de5986ad451a70e1fccc2c7a521a9b72f3eb98e
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-17 09:12:38 -0600

    Funciona sesiones básicas

--- frontend-controladora-js/src/components/Login.vue
+++ frontend-controladora-js/src/components/Login.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="row">
-        <form class="mx-auto col-sm-3 ">
+        <div class="mx-auto col-sm-3 ">
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" v-model="email" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <label for="exampleInputEmail1">Username</label>
+                <input type="text" class="form-control" v-model="email" aria-describedby="emailHelp" placeholder="Enter username">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your data with anyone else.</small>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" v-model="password" placeholder="Password">
+                <input type="password" class="form-control" v-model="password" placeholder="Password" v-on:keyup.enter="login">
             </div>
-            <button type="submit" class="btn btn-primary" @click="login">Login</button>
-        </form>
+            <button type="button" class="btn btn-primary" @click="login">Login</button>
+        </div>
     </div>
 
 
@@ -29,23 +29,23 @@ import SessionDataService from "../services/SessionDataService";
       };
     },
     methods: {
-      login(){
+      async login(){
         var data = {
           email: this.email,
           password: this.password
         }
-        SessionDataService.login(data)
-          .then(response => {
-            if (response.status === 200 && 'username' in response.data) {
-              sessionStorage.setItem("token", "fewuoibfeiwbslwl")
-              this.$router.push('/backups')
-            }
-          })
-          .catch(e => {
-            console.log(e);
-        });
-      },
-
+        await SessionDataService.login(data)
+            .then(response => {
+              if (response.status === 200 && 'username' in response.data) {
+                console.log(response.data);
+                sessionStorage.setItem("token", "fewuoibfeiwbslwl");
+                this.$router.push('/backups');
+              }
+            })
+            .catch(e => {
+              console.log(e);
+          });
+      }
     }
   };
   </script>
