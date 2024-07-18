83672326af2b479659d9c5f448db4f16f85835ba
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-18 17:50:53 -0600

    Prueba cors nginx

--- frontend-controladora-js/src/http-common.js
+++ frontend-controladora-js/src/http-common.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_ABKP_AXIOS_BASEURL,
+  //baseURL: process.env.VUE_APP_ABKP_AXIOS_BASEURL,
   //baseURL: "http://10.22.165.29:8080/api",
   //baseURL: "http://auto-backup-express-1:8080/api",
+  baseURL: "auto-backup-express-git:8080/api",
   
   headers: {
     //"Content-type": "application/json"
