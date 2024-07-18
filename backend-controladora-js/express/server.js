1b4b9708cb258b75e19ddcd84086514609dfdd01
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-18 15:26:47 -0600

    prueba cors

--- backend-controladora-js/express/server.js
+++ backend-controladora-js/express/server.js
@@ -7,15 +7,19 @@ var corsOptions = {
   //Esto debe referenciarse a sí mismo
   //"origin": "http://auto-backup-vuejs-1:8081"
   //"origin": process.env.EXPRESS_CORS_OPTIONS
-  origin: 'https://auto-backup-vuejs-git-or15.apps.ocpprod.pjedomex.gob.mx/'
+  'origin': 'https://auto-backup-vuejs-git-or15.apps.ocpprod.pjedomex.gob.mx/',
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Allow-Headers':'Content-Type'
 };
 
 //Cors para un origen específico
-//console.log(corsOptions.origin);
-//app.use(cors(corsOptions));
+console.log(corsOptions.origin);
+app.use(cors(corsOptions));
 
 //Cors para cualquier origen desconocido
-app.use(cors());
+//app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
