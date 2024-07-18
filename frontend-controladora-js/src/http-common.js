e53919e787a947f17fb7d9e2ba395e4159e821f2
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-18 12:33:40 -0600

    ACAH: Content-Type

--- frontend-controladora-js/src/http-common.js
+++ frontend-controladora-js/src/http-common.js
@@ -8,6 +8,6 @@ export default axios.create({
   headers: {
     //"Content-type": "application/json"
     'Content-Type': 'text/plain;charset=utf-8',
-    "Access-Control-Allow-Origin": "*"
+    'Access-Control-Allow-Headers':'Content-Type'
   }
 });
