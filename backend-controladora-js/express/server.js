e85c693254a1e57f553c936907b058f963d12293
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-19 12:24:52 -0600

    Antes de la modificacion del flask

--- backend-controladora-js/express/server.js
+++ backend-controladora-js/express/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-var whitelist = ['https://auto-backup-vuejs-git-or15.apps.ocpprod.pjedomex.gob.mx', 'http://auto-backup-vuejs-git:8080']
+var whitelist = ['https://auto-backup-vuejs-git-or15.apps.ocpprod.pjedomex.gob.mx', 'http://auto-backup-vuejs-git:8080', 'http://10.22.165.29:8080', 'http://auto-backup-vuejs-1:8081']
 
 var corsOptions = {
   origin: function (origin, callback) {
--- frontend-controladora-js/src/http-common.js
+++ frontend-controladora-js/src/http-common.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: process.env.VUE_APP_ABKP_AXIOS_BASEURL,
-  //baseURL: "http://10.22.165.29:8080/api",
+  baseURL: "http://10.22.165.29:8080/api",
   //baseURL: "http://auto-backup-express-1:8080/api",
-  baseURL: "http://auto-backup-express-git:8080/api",
+  //baseURL: "http://auto-backup-express-git:8080/api",
   
   headers: {
     //"Content-type": "application/json"
     //'Origin':'https://auto-backup-vuejs-git-or15.apps.ocpprod.pjedomex.gob.mx',
     'Content-Type': 'application/json; charset=utf-8',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers':'Content-Type'
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Methods': '*',
+    // 'Access-Control-Allow-Headers':'Content-Type'
   }
 });
