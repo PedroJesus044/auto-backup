8bd70ae24eb3f355835c81b94d7be45274428340
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-22 11:11:00 -0600

    axios

--- frontend-controladora-js/src/http-common.js
+++ frontend-controladora-js/src/http-common.js
@@ -4,7 +4,7 @@ export default axios.create({
   //baseURL: process.env.VUE_APP_ABKP_AXIOS_BASEURL,
   //baseURL: "http://10.22.165.29:8080/api",
   //baseURL: "http://auto-backup-express-1:8080/api",
-  baseURL: "https://auto-backup-express-git-pibarrap044-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/",
+  baseURL: "https://auto-backup-express-git-pibarrap044-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/api",
   
   headers: {
     //"Content-type": "application/json"
